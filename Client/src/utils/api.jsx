c213cbf2ed985eb5d0d2a3a069663b5487965daf
TanviSharma2104 <tanvisharma2703@gmail.com>
2024-03-15 19:48:06 +0530

    added backend production link

--- Client/src/utils/api.jsx
+++ Client/src/utils/api.jsx
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const Mentor_Id = "65f3fcd5679e5bc6f48ec4a2";
 export const privateAxios = axios.create({
-    baseURL: "http://localhost:4000"
+    baseURL: "https://mentor-dashboard-pied.vercel.app"
 });
